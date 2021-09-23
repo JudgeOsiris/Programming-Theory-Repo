0ba8fb8aac25dbc4fe0b4b812e9284a6242c06f9
JudgeOsiris <70705183+JudgeOsiris@users.noreply.github.com>
2021-09-23 23:02:49 +0300

    Create scripts: Animal, Fox and Wolf

---
+++ Programming Theory Project/Assets/Scripts/TitleUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    public void StartProject()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
